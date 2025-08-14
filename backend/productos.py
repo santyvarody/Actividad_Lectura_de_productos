494ed625d45f0f41f0bff0bf796d21c277f73231
Santiago Vargas <santyvarody@gmail.com>
2025-08-14 17:27:40 -0500

    Se añade función de crear y eliminar productor

--- backend/productos.py
+++ backend/productos.py
@@ -1,4 +1,5 @@
 from backend.hoja_productos import obtenerHojaDeProductos
+from backend.excel import guardarHoja
 
 hoja = obtenerHojaDeProductos()
 
@@ -17,12 +18,12 @@ def listarProductos():
 
     return filas
 
-def consultarProducto(documento):
+def consultarProducto(id):
     refFilas = hoja.iter_rows(min_row=2, max_row=hoja.max_row, min_col=1, max_col=4)
     refFilasEnum = enumerate(refFilas)
 
     for idx, refFila in refFilasEnum:
-        if refFila[0].value == documento:
+        if refFila[0].value == id:
             valores = []
             valores.append(idx)
 
@@ -32,3 +33,27 @@ def consultarProducto(documento):
             return valores
     else:
         return None            
+
+def crearProducto(id, nombre, precio, cantidad):
+    if consultarProducto(id) != None:
+      return False
+    
+    producto = (id, nombre, precio, cantidad)
+
+    hoja.append(producto)
+
+    guardarHoja(hoja)
+
+    return True
+
+def eliminarProducto(id):
+    producto = consultarProducto(id)
+
+    if producto == None:
+        return False
+    
+    hoja.delete_rows(producto[0]+2)
+
+    guardarHoja(hoja)
+
+    return True
---
+++ main.py
@@ -0,0 +1,4 @@
+from backend.productos import crearProducto, eliminarProducto
+
+crearProducto(50, "Azucar", 6000, 30)
+eliminarProducto(40)
